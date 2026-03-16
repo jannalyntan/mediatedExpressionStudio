105bfdad27804d5c645962f470c727e2a5de46d6
jannalyntan <s4056775@student.rmit.edu.au>
2026-03-16 14:52:05 +1100

    update

---
+++ in class test/tone_test/main.js
@@ -0,0 +1,10 @@
+// find elements from the code
+const introDialog = document.querySelector("intro-dialog");
+console.log(introDialog);
+const introDialogClose = document.querySelector("dialog-close-btn");
+
+// Intro Dialog Setup
+
+introDialog.showModal();
+
+introDialogClose.addEventListener("click");
